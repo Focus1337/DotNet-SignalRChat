158f401545edb2de962a51637e1b33675e7b0c8d
Focus <maniaclord@bk.ru>
2023-04-18 16:39:23 +0300

    Project init

---
+++ SignalRChat/Hubs/ChatHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using SignalRChat.Hubs.Clients;
+
+namespace SignalRChat.Hubs;
+
+public class ChatHub : Hub<IChatClient>
+{
+    public async Task SendMessage(string name, string text)
+    {
+        await Clients.All.ReceiveMessage(name, text);
+    }
+}
---
+++ SignalRChat/Hubs/Clients/IChatClient.cs
@@ -0,0 +1,6 @@
+﻿namespace SignalRChat.Hubs.Clients;
+
+public interface IChatClient
+{
+    public Task ReceiveMessage(string name, string text);
+}
