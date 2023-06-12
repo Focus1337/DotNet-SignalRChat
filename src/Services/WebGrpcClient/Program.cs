624dfef2d941f0d9438d563320dfeeb297836ffe
Focus <screamfocus@gmail.com>
2023-06-12 14:34:29 +0300

    temp com

--- src/Services/WebGrpcClient/Program.cs
+++ src/Services/WebGrpcClient/Program.cs
@@ -1,4 +1,5 @@
 using Grpc.Core;
+using Grpc.Net.Client.Configuration;
 using WebGrpcClient;
 using WebGrpcClient.Services;
 
@@ -8,7 +9,25 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddGrpcClient<Greeter.GreeterClient>("WebClient",
-        opts => { opts.Address = new Uri("http://localhost:5029"); })
+        opts => opts.Address = new Uri("http://localhost:5029"))
+    .ConfigureChannel(opts => opts.ServiceConfig = new ServiceConfig
+    {
+        MethodConfigs =
+        {
+            new MethodConfig
+            {
+                Names = { MethodName.Default },
+                RetryPolicy = new RetryPolicy
+                {
+                    MaxAttempts = 5,
+                    InitialBackoff = TimeSpan.FromSeconds(1),
+                    MaxBackoff = TimeSpan.FromSeconds(5),
+                    BackoffMultiplier = 1.5,
+                    RetryableStatusCodes = { StatusCode.Unavailable }
+                }
+            }
+        }
+    })
     .EnableCallContextPropagation();
 
 builder.Services.AddTransient<GreeterClientService>();
